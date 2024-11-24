13a3806f13d0d13a25f7b6c519da6aa15e4069ea
Rohit Mahant <rohitsinghmahant707@gmail.com>
2024-11-24 11:25:26 -0500

    testing CI/CD

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App text-sm font-sans text-black">
-     Hello This is sample node application
+     Hello This is sample node application .
+     <span className="text-lg font-bold ">I am Supreme</span>
     </div>
   );
 }
